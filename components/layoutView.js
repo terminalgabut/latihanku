8ce69d7637662acbc12a47cd58d948af24fc8aba
terminalgabut <trasipahit02@gmail.com>
2026-09-12 23:01:24 +0700

    Update layoutView.js

--- components/layoutView.js
+++ components/layoutView.js
@@ -1,38 +1,22 @@
-// components/layoutView.js
-
-export default {
-  name: 'LayoutView',
-  template: `
-    <div id="app" class="app-layout">
-        
-        <!-- Header Sticky -->
+export default `
+    <div class="app-layout" :class="{ 'sidebar-open': isSidebarOpen }">
+        <!-- Header Fixed Top -->
         <header-component @toggle-sidebar="toggleSidebar"></header-component>
 
-        <!-- App Container (Sidebar + Content) -->
-        <div class="app-container">
-            
-            <!-- Mobile Overlay Slide-over -->
-            <div 
-                class="sidebar-overlay" 
-                :class="{ 'is-active': isSidebarOpen }"
-                @click="toggleSidebar"
-            ></div>
+        <!-- Sidebar Drawer -->
+        <sidebar-component 
+            :is-open="isSidebarOpen"
+            @close-sidebar="toggleSidebar"
+        ></sidebar-component>
 
-            <!-- Sidebar Drawer -->
-            <sidebar-component 
-                :is-open="isSidebarOpen"
-                @close-sidebar="toggleSidebar"
-            ></sidebar-component>
-
-            <!-- Main Content Area -->
-            <main class="main-content">
+        <!-- Main Viewport Area -->
+        <div class="app-viewport">
+            <main class="app-content">
                 <div class="view-container">
                     <router-view></router-view>
                 </div>
             </main>
-
         </div>
     </div>
-  
 `
-};
+  ;
