39aa48940fcb091764a0941e76f7de9f93a40888
terminalgabut <trasipahit02@gmail.com>
2026-09-12 23:07:44 +0700

    Update sidebar.js

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,17 +1,15 @@
-// js/components/sidebar.js
-import sidebarView from './sidebarView.js';
+// components/sidebar.js
+import sidebarTemplate from './sidebarView.js';
 
 export default {
-    ...sidebarView,
-    emits: ['close-sidebar'],
-    mounted() {
-        if (window.lucide && typeof window.lucide.createIcons === 'function') {
-            window.lucide.createIcons();
+    name: 'SidebarComponent',
+    template: sidebarTemplate,
+    props: {
+        isOpen: {
+            type: Boolean,
+            default: false
         }
     },
-    updated() {
-        if (window.lucide && typeof window.lucide.createIcons === 'function') {
-            window.lucide.createIcons();
-        }
-    }
-};
+    emits: ['close-sidebar']
+}
+    ;
